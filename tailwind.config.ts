0330ae86304aaf2288f24eaaa26903b403370bf1
CyprienGille <>
2024-07-12 15:03:49 +0200

    Opt in theme enhancements (for background gradients)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,12 @@
 import { join } from 'path';
 import type { Config } from 'tailwindcss';
 
-// 1. Import the Skeleton plugin
 import { skeleton } from '@skeletonlabs/tw-plugin';
 
 const config = {
-  // 2. Opt for dark mode to be handled via the class method
   darkMode: 'class',
   content: [
     './src/**/*.{html,js,svelte,ts}',
-    // 3. Append the path to the Skeleton package
     join(require.resolve(
       '@skeletonlabs/skeleton'),
       '../**/*.{html,js,svelte,ts}'
@@ -20,9 +17,21 @@ const config = {
     extend: {},
   },
   plugins: [
-    // 4. Append the Skeleton plugin (after other plugins)
     skeleton({
-      themes: { preset: ["gold-nouveau", "modern", "skeleton"] }
+      themes: { preset: [
+        {
+          name: 'skeleton',
+          enhancements: true,
+        },
+        {
+          name: 'gold-nouveau',
+          enhancements: true,
+        },
+        {
+          name: 'modern',
+          enhancements: true,
+        },
+      ] }
     })
   ]
 } satisfies Config;
