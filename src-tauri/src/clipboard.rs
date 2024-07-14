75f9917893e1d0e26b101cfd592d364b2617d827
CyprienGille <>
2024-07-15 01:49:11 +0200

    Factorization and clearer method

--- src-tauri/src/clipboard.rs
+++ src-tauri/src/clipboard.rs
@@ -4,10 +4,11 @@ pub fn get_clipboard() -> String {
     if let Ok(mut cb) = Clipboard::new() {
         cb.get_text().unwrap_or_default()
     } else {
-        "".to_owned()
+        "".to_string()
     }
 }
 
+#[tauri::command]
 pub fn set_clipboard(content: &str) {
     match Clipboard::new() {
         Ok(mut cb) => {
