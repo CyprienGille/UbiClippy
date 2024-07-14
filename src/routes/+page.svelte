e61e9512fd8a124ec968b338f0d4a298463cd3c2
CyprienGille <>
2024-07-14 16:54:51 +0200

    Temporary UI for copying response to clipboard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,21 +3,26 @@
 	import { listen } from '@tauri-apps/api/event';
 
 	let last_response = '';
-	// "In this example, any requests starting with /ubiclippy will be proxied to the Tauri backend running at tauri://localhost/. Make sure to adjust the configuration to match your specific setup. Choose an approach that best fits your project's requirements and constraints, and remember to test thoroughly in both development and production environments to ensure everything works as expected.";
 
-	const unlisten = listen('ollama_chunk', (event: any) => {
+	const unlisten = listen('llm_chunk', (event: any) => {
 		last_response += event.payload.response;
 	});
 
 	function trigger_message() {
 		console.log('Pressed button...');
-		invoke('trigger_response');
+		invoke('prompt_with_cb', { model: 'llama3:instruct' });
+	}
+
+	function into_clipboard(response: string) {
+		invoke('response_to_clipboard', { response });
 	}
 </script>
 
 <main class="h-screen w-screen content-center space-y-5">
 	<div class="w-full text-center">
-		<button class="btn variant-filled-primary" on:click={trigger_message}> Who are you? </button>
+		<button class="btn variant-filled-primary" on:click={trigger_message}>
+			Summarize Clipboard
+		</button>
 	</div>
 	<div class="w-full flex">
 		<div class="w-1/4"></div>
@@ -27,5 +32,10 @@
 				<span>{last_response}</span>
 			</div>
 		</div>
+	</div>
+	<div class="w-full text-center">
+		<button class="btn variant-filled-secondary" on:click={() => into_clipboard(last_response)}>
+			Copy resp to clipboard
+		</button>
 	</div>
 </main>
