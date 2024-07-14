30ebfc408865ad4851c219cd7f924c511fbd55d8
CyprienGille <>
2024-07-14 16:50:58 +0200

    Implement writing and reading the clipboard

---
+++ src-tauri/src/clipboard.rs
@@ -0,0 +1,20 @@
+use arboard::Clipboard;
+
+pub fn get_clipboard() -> String {
+    if let Ok(mut cb) = Clipboard::new() {
+        cb.get_text().unwrap_or_default()
+    } else {
+        "".to_owned()
+    }
+}
+
+pub fn set_clipboard(content: &str) {
+    match Clipboard::new() {
+        Ok(mut cb) => {
+            if let Err(e) = cb.set_text(content) {
+                eprintln!("Failed to write to the clipboard: {}", e);
+            }
+        }
+        Err(e) => eprintln!("Failed to access the clipboard: {}", e),
+    }
+}
