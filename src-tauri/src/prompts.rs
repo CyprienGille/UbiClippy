9c9b365b9f93943be4b90972a7794f79c6f64ca0
CyprienGille <83751996+CyprienGille@users.noreply.github.com>
2024-07-30 22:45:50 +0200

    Add optional string trigger to prompts

--- src-tauri/src/prompts.rs
+++ src-tauri/src/prompts.rs
@@ -19,14 +19,16 @@ pub struct Prompt {
     id: u64,
     content: String,
     enabled: bool,
+    trigger: Option<String>,
 }
 
 impl Prompt {
-    fn new(id: u64, content: String, enabled: bool) -> Self {
+    fn new(id: u64, content: String, enabled: bool, trigger: Option<String>) -> Self {
         Prompt {
             id,
             content,
             enabled,
+            trigger,
         }
     }
 }
@@ -45,40 +47,56 @@ impl PromptLib {
     pub fn defaults() -> Self {
         PromptLib {
             prompts: Mutex::new(vec![
-                Prompt::new(0, "Hi!".to_string(), true),
+                Prompt::new(0, "Hi!".to_string(), true, Some("h".to_string())),
                 Prompt::new(
                     1,
                     "Who are you? Answer in a single sentence.".to_string(),
-                    true,
+                    true, 
+                    Some("w".to_string())
                 ),
                 Prompt::new(
                     2,
                     "Summarize the following text in two sentences. Do not say 'Here is a summary of the text'.\n$CLIPBOARD$".to_string(),
-                    true,
+                    true, 
+                    Some("s".to_string())
                 ),
                 Prompt::new(
                     3,
                     "What is the following about?\n$CLIPBOARD$".to_string(),
-                    true,
+                    true, 
+                    None
                 ),
                 Prompt::new(
                     4,
                     "Format this nicely.\n$CLIPBOARD$".to_string(),
-                    true,
+                    true, 
+                    None
                 ),
                 Prompt::new(
                     5,
                     "What does this code do?.\n$CLIPBOARD$".to_string(),
-                    true,
+                    true, 
+                    None
                 ),
+                Prompt::new(
+                    6,
+                    "Is this my first message to you?".to_string(),
+                    true, 
+                    Some("i".to_string())
+                )
             ]),
         }
     }
 }
 
 #[tauri::command]
-pub fn add_prompt(new_prompt: String, all_prompts: State<PromptLib>, id_state: State<IdState>) {
-    let prompt = Prompt::new(id_state.consume_free_id(), new_prompt, true);
+pub fn add_prompt(
+    new_prompt: String,
+    trigger: Option<String>,
+    all_prompts: State<PromptLib>,
+    id_state: State<IdState>,
+) {
+    let prompt = Prompt::new(id_state.consume_free_id(), new_prompt, true, trigger);
     all_prompts.prompts.lock().unwrap().push(prompt);
 }
 
