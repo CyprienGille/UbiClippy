8aaa66741e91325371cac6c9d84f65d4c8c6e4c8
CyprienGille <83751996+CyprienGille@users.noreply.github.com>
2024-08-05 19:49:11 +0200

    Add basic settings list

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,26 @@
+<main class="h-screen w-full">
+	<ol class="breadcrumb pl-3 pt-2 text-xl">
+		<li class="crumb"><a class="anchor" href="/">Home</a></li>
+		<li class="crumb-separator" aria-hidden="true">&rsaquo;</li>
+		<li>Settings</li>
+	</ol>
+
+	<div class="flex h-3/4 w-full items-center justify-center">
+		<nav class="list-nav flex w-2/3 flex-col justify-center bg-secondary-500/30 text-2xl">
+			<ul>
+				<li>
+					<a href="/settings/prompts/">
+						<span class="badge mr-2 bg-primary-500">(icon)</span>
+						<span class="flex-auto">Prompt Library</span>
+					</a>
+				</li>
+				<li>
+					<a href="/settings/prompts/">
+						<span class="badge mr-2 bg-primary-500">(icon)</span>
+						<span class="flex-auto">Prompt Library</span>
+					</a>
+				</li>
+			</ul>
+		</nav>
+	</div>
+</main>
